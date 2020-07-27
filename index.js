6a4e315f4f874842bde085907153834a9dda3d17
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-27 23:14:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function map(Array  , fun) {
    return Array.map(x => fun(x));
 }
 
-function reduce(saleem ,func ) {
-  if (startingPoint => 0){
+function reduce(saleem ,func  ,startingPoint ) {
+  if (startingPoint > 0 ){
     return startingPoint + saleem.reduce(func) ;
-  }else if  {
-    return saleem.reduce(func , true);
+  }else   {
+    return saleem.reduce(func);
   }
 
 }
